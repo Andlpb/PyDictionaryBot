b5fb846911db512fa93829c513c434b5a503010e
Andrew <123967804+Andlpb@users.noreply.github.com>
2025-01-24 10:33:50 +0500

    Update telegram-bot.py
    Add a language model

--- telegram-bot.py
+++ telegram-bot.py
@@ -1,14 +1,26 @@
 from config import TELEGRAM_BOT_TOKEN
-from python_reference import python_help_dict
+from python_reference import python_help_dict, PROMPT_LM
 from bot import PythonHelperBot, NOT_FOUND_MASSAGE
 
+import languagemodels as lm
 from telegram import Update
 from telegram.ext import Application, MessageHandler
 
+lm.config["max_ram"] = "4gb"
+
 bot=PythonHelperBot(python_help_dict)
+
+def lm_chat(user_query):
+    prompt = f'{PROMPT_LM}\nUser: {user_query} \nAssistant:'
+    result = lm.chat(prompt)
+    return result
+
+
 async def helper(update, context):
     user_message = update.message.text
     answer=bot.get_help(user_message)
+    if answer == NOT_FOUND_MASSAGE:
+        answer = lm_chat(user_message)
     await update.message.reply_text(answer)
 
 def run_bot():
